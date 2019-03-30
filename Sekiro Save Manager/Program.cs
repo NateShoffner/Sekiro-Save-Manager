a6139230956c7b98b48a7700c99194e17f6a5c1c
Nate Shoffner <nate.shoffner@gmail.com>
2019-03-30 12:09:46 -0400

    Portable settings

--- Sekiro Save Manager/Program.cs
+++ Sekiro Save Manager/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Configuration;
 using System.Windows.Forms;
 using Sekiro_Save_Manager.Forms;
+using Sekiro_Save_Manager.Properties;
 
 namespace Sekiro_Save_Manager
 {
@@ -12,9 +14,22 @@ namespace Sekiro_Save_Manager
         [STAThread]
         private static void Main()
         {
+            InitializeSettings();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        public static void InitializeSettings()
+        {
+            Settings.Default.Upgrade();
+            var provider = new PortableSettingsProvider();
+            Settings.Default.Providers.Add(provider);
+            foreach (SettingsProperty property in Settings.Default.Properties)
+            {
+                property.Provider = provider;
+            }
         }
     }
 }
